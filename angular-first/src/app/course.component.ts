365b327419ecbd573e87b8bdbac7c4e51d3a2cd7
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-08-25 11:56:40 -0700

    generate course component

--- angular-first/src/app/course.component.ts
+++ angular-first/src/app/course.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
-
-@Component({ selector: 'courses', template: '<h2>Courses</h2>' })
+//html markup- template
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>',
+})
 export class CourseComponent {}
